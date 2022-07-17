5d3bcb9cc208c504c88c2dcccf6e509c8756e7a3
sschreyer <samschreyer360@gmail.com>
2022-07-17 20:09:26 +1000

    Basic random npc spawns implemented

---
+++ Assets/Scripts/NPCSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject npc;
+
+    void Start() {
+        int numChildren = transform.childCount;
+        Transform[] children = gameObject.GetComponentsInChildren<Transform>();
+    
+        List<int> prevNumbers = new List<int>();
+        // we make 
+        for (int i = 0; i < 4; i++) {
+            int n = Random.Range(0, numChildren);
+            while (prevNumbers.Contains(n)) {
+                n = Random.Range(0, numChildren);
+            }
+            prevNumbers.Add(n);
+            // set it to the location of the nth child
+            GameObject newNpc = Instantiate(npc, children[n]);
+        }   
+    } 
+}
