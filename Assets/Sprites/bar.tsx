76ae25061023f63304863f5a8539090788e06a52
Corrade <49605314+Corrade@users.noreply.github.com>
2022-07-17 14:34:36 +1000

    Uploaded draft tileset
    The tileset was created using Tiled, which has a Unity plugin.
    bar.tmx is the main project file, bar.tsx is the tileset, and bar.png is the image source for the tileset. They reference each other in that order (you may have to tidy up the references).
    bar.tmx contains four layers. Three are cosmetic and one is a collision map. The collision map uses two collision tiles - one full tile, and one partial tile.

---
+++ Assets/Sprites/bar.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="gmtk" tilewidth="150" tileheight="150" tilecount="676" columns="26">
+ <image source="gmtk.png" width="3975" height="3975"/>
+</tileset>
